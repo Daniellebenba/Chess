ea06d344b7c86d85b8bbb44ecddafffd6fe7230a
Yonit <za.yonit@gmail.com>
2017-09-24 19:12:57 +0300

    Added some "instructions" in the main.h
    bla bla

--- main.h
+++ main.h
@@ -9,5 +9,24 @@
 #define MAIN_H_
 
 
-
 #endif /* MAIN_H_ */
+/*
+Alright now what.
+see here <- Chess master <- that means that you're in the main branch
+oK
+now, if you make any changes you'll need to :
+1.to pull the main branch (master) so behind your stuff there's the latest and greatest version
+2.commit to your changes, give them a name,
+3. Push - merge it with the master branch (ma ze omer?!) (it means that if now you and Danielle made
+changes on the same file, and Danielle pushed before you did, you'll have to make sure there are no collisions
+with Danielle changes, and if there are change you'll need to decide which version do you keep.
+this way, both of you can work in parallel and keep the code saef from BUGGGGS
+
+it is inportant that from now on you do whatever you do on the Git folder. OK?
+yes
+
+now, once we see that its a good environement for you, you'll help her have the same :)
+
+alright, enough with talking, lets see what happened with the changes we did,
+so first, cntrl+S to get rid of the *
+*/
